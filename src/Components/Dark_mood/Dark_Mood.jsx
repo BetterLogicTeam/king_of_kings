b9e2dc264a1352c59bd348698b496f76ceed78a7
BetterLogic Team <102576102+BetterLogicTeam@users.noreply.github.com>
2022-10-15 13:44:25 +0500

    Final Commint Video

--- src/Components/Dark_mood/Dark_Mood.jsx
+++ src/Components/Dark_mood/Dark_Mood.jsx
@@ -1,85 +1,98 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Dark from "../Assets/Dark.png"
 import Landing from '../Landing/Landing';
+import { Player } from 'video-react';
+// import "node_modules/video-react/dist/video-react.css"; // import css
 
 
 
 export default function Dark_Mood() {
     let [value, setValue] = useState(1);
-let navigate=useNavigate()
+    let navigate = useNavigate()
     const increaseValue = () => {
-      if (value < 10) {
-        setValue(++value);
-        console.log("setValue", value);
-      }
+        if (value < 10) {
+            setValue(++value);
+            console.log("setValue", value);
+        }
     };
-  
+
     const decreaseValue = () => {
-      if (value > 1) {
-        setValue(--value);
-        console.log("setValue", value);
-      }
+        if (value > 1) {
+            setValue(--value);
+            console.log("setValue", value);
+        }
     };
-  return (
-    <div>
+useEffect(() => {
+    
+    document.getElementById('vid').play();
 
-<div className='main_div'>
-        <div className="">
-           <div className="container kig">
-            <div className="row">
-                <div className="col-lg-2"></div>
-            <div className="col-lg-7 d-flex responsive">
-            <div className="row justify-content-center">
-                <div className="col-md-6 responsive">
-                <div className="vid_div">
-                <video src="Dark.mp4" className='w-100 '></video>
+}, [])
 
-            </div>
+    
+    return (
+        <div>
+
+            <div className='main_div'>
+                <div className="">
+                    <div className="container kig">
+                        <div className="row">
+                            <div className="col-lg-2"></div>
+                            <div className="col-lg-7 d-flex responsive">
+                                <div className="row justify-content-center">
+                                    <div className="col-md-6 responsive">
+                                        <div className="vid_div">
+                                            {/* <video src="Dark.mp4" className='w-100 '></video> */}
+                                            <video  autoplay="autoplay" loop width="100%" id='vid'>
+                                                <source src="Dark.mp4" type="video/mp4" />
+
+                                            </video>
+
+                                        </div>
+                                    </div>
+
+                                    <div className="col-md-6 responsive scnd">
+                                        <div className="logo">
+                                            <img src="logo.png" alt="" />
+                                        </div>
+
+                                        <div className="bttn">
+                                            <div className="btn fst_bttn">BNB</div>
+                                            <div className="btn fst_bttn">ETH</div>
+                                        </div>
+
+                                        <div className="heding">
+                                            <h4 className='text-white pt-3'>GENESIS KING CROWN</h4>
+                                            <p>0.4 BNB</p>
+                                        </div>
+
+                                        <div className="scnd_emg">
+                                            <img src={Dark} alt="" />
+                                        </div>
+
+                                        <div className="bttn">
+                                            <button className="btn scnd_bttn activedark1 " onClick={() => navigate('/')}>LIGHT THEME</button>
+                                            <button className="btn scnd_bttn activedark activedark1" onClick={() => navigate('/Dark_Mood')}>DARK THEME</button>
+                                        </div>
+
+                                        <div className="blck">
+                                            <div className="btn plus" onClick={() => decreaseValue()}>-</div>{" "}
+                                            <input type="text"
+                                                value={value}
+                                                onChange={(e) => setValue(e.target.value)} />{" "}
+                                            <div className="btn plus" onClick={() => increaseValue()}>+</div>{" "}
+                                        </div>
+
+                                        <div className="mint">
+                                            <img src="mint.png" alt="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="col-md-6 responsive scnd">
-                    <div className="logo">
-                        <img src="logo.png" alt="" />
-                    </div>
-
-                    <div className="bttn">
-                        <div className="btn fst_bttn">BNB</div>
-                        <div className="btn fst_bttn">ETH</div>
-                    </div>
-
-                    <div className="heding">
-                        <h4 className='text-white pt-3'>GENESIS KING CROWN</h4>
-                        <p>0.4 BNB</p>
-                    </div>
-
-                    <div className="scnd_emg">
-                        <img src={Dark} alt="" />
-                    </div>
-
-                    <div className="bttn">
-                        <button className="btn scnd_bttn activedark1 " onClick={()=> navigate('/')}>LIGHT THEME</button>
-                        <button className="btn scnd_bttn activedark activedark1" onClick={()=>navigate('/Dark_Mood')}>DARK THEME</button>
-                    </div>
-
-                    <div className="blck">
-                        <div className="btn plus" onClick={() => decreaseValue()}>-</div>{" "}
-                        <input type="text" 
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}/>{" "}
-                        <div className="btn plus" onClick={() => increaseValue()}>+</div>{" "}
-                    </div>
-
-                    <div className="mint">
-                        <img src="mint.png" alt="" />
-                    </div>
-                </div>
             </div>
-            </div>
-            </div>
-           </div>
         </div>
-    </div>
-    </div>
-  )
+    )
 }
--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Landing.css';
 import card from "../Assets/card.png"
 import KingLogo from "../Assets/logo.png"
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 function Landing() {
   let [value, setValue] = useState(1);
 
-  let navigate=useNavigate()
+  let navigate = useNavigate()
   const increaseValue = () => {
     if (value < 10) {
       setValue(++value);
@@ -25,67 +25,79 @@ function Landing() {
       console.log("setValue", value);
     }
   };
+  setInterval(() => {
+      
+    document.getElementById('vid').play();
+  }, 1000);
 
   return (
     <>
-    <div className='main_div'>
+      <div className='main_div'>
         <div className="">
-           <div className="container kig">
+          <div className="container kig">
             <div className="row">
-                <div className="col-lg-2"></div>
-            <div className="col-lg-7 d-flex responsive">
-            <div className="row justify-content-center">
-                <div className="col-md-6 responsive">
-                <div className="vid_div">
-                <video src="light.mp4" className='w-100 '></video>
+              <div className="col-lg-2"></div>
+              <div className="col-lg-7 d-flex responsive">
+                <div className="row justify-content-center">
+                  <div className="col-md-6 responsive">
+                    <div className="vid_div">
+                      {/* <video src="light.mp4" className='w-100 '></video> */}
+                      <div className="vid_div">
+                        {/* <video src="Dark.mp4" className='w-100 '></video> */}
+                        <video autoplay="autoplay" loop width="100%" id='vid'>
+                          <source src="light.mp4" type="video/mp4" />
 
-            </div>
-                </div>
+                        </video>
 
-                <div className="col-md-6 responsive scnd">
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div className="col-md-6 responsive scnd">
                     <div className="logo">
-                        <img src="logo.png" alt="" />
+                      <img src="logo.png" alt="" />
                     </div>
 
                     <div className="bttn">
-                        <div className="btn fst_bttn">BNB</div>
-                        <div className="btn fst_bttn">ETH</div>
+                      <div className="btn fst_bttn">BNB</div>
+                      <div className="btn fst_bttn">ETH</div>
                     </div>
 
                     <div className="heding">
-                        <h4 className='text-white pt-3'>GENESIS KING CROWN</h4>
-                        <p>0.4 BNB</p>
+                      <h4 className='text-white pt-3'>GENESIS KING CROWN</h4>
+                      <p>0.4 BNB</p>
                     </div>
 
                     <div className="scnd_emg">
-                        <img src="LIGHT.png" alt="" />
+                      <img src="LIGHT.png" alt="" />
                     </div>
 
                     <div className="bttn">
-                        <div className="btn scnd_bttn actiggve" onClick={()=> navigate('/')}>LIGHT THEME</div>
-                        <div className="btn scnd_bttn" onClick={()=> navigate('/Dark_Mood')}>DARK THEME</div>
+                      <div className="btn scnd_bttn actiggve" onClick={() => navigate('/')}>LIGHT THEME</div>
+                      <div className="btn scnd_bttn" onClick={() => navigate('/Dark_Mood')}>DARK THEME</div>
                     </div>
 
                     <div className="blck">
-                        <div className="btn plus" onClick={() => decreaseValue()}>-</div>{" "}
-                        <input type="text" 
+                      <div className="btn plus" onClick={() => decreaseValue()}>-</div>{" "}
+                      <input type="text"
                         value={value}
-                        onChange={(e) => setValue(e.target.value)}/>{" "}
-                        <div className="btn plus" onClick={() => increaseValue()}>+</div>{" "}
+                        onChange={(e) => setValue(e.target.value)} />{" "}
+                      <div className="btn plus" onClick={() => increaseValue()}>+</div>{" "}
                     </div>
 
                     <div className="mint">
-                        <img src="mint.png" alt="" />
+                      <img src="mint.png" alt="" />
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-            </div>
-            </div>
-           </div>
+          </div>
         </div>
-    </div>
+      </div>
 
-    
+
 
 
     </>
